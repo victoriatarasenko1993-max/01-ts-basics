37b308009acb8bd9d47a2f84b843f7de2328afbb
victoriatarasenko1993-max <victoriatarasenko1993@gmail.com>
2026-06-12 01:18:52 +0300

    hw1ts

--- src/task-1.ts
+++ src/task-1.ts
@@ -1,6 +1,6 @@
-const name = "Alice";
-const age = 30;
-const isOnline = true;
+const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
 
 console.log(`Name: ${name}`);
 console.log(`Age: ${age}`);
